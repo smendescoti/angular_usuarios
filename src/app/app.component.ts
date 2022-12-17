158cbdc8bdcc021e45a8f6d67d027855d5cbfc2e
smendescoti <sergio.coti@gmail.com>
2022-12-17 12:25:47 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'projeto_angular02';
+export class AppComponent implements OnInit {
+
+  //atributos
+  isAuthenticated: boolean = false;
+  emailUsuario: string = '';
+
+  //evento executado sempre que a página é carregada
+  ngOnInit(): void {
+
+    //capturar o token do usuário salvo na localstorage
+    var token = localStorage.getItem('access-token');
+
+    if(token != null){
+
+      this.isAuthenticated = true;
+
+      let email = localStorage.getItem('email_usuario');
+      if(email != null) {
+        this.emailUsuario = email;
+      }
+    }
+  }
+
+  //função para realizar o logout do usuário
+  logout(): void {
+
+    if(window.confirm('Deseja realmente sair da aplicação?')) {
+      
+      //apagar os dados gravados na local storage
+      localStorage.removeItem('access-token');
+      localStorage.removeItem('email_usuario');
+
+      //redirecionar de volta para a página de login
+      window.location.href = "/acessar-conta";
+    }
+  }
+
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    /* endereço base da API */
+    API_URL: 'http://apiusuarios-env.eba-qwym3m9c.us-east-1.elasticbeanstalk.com'
+}
